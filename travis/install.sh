d166cf6539377d38dcc87c2b9a4bd9f0bcf707c7
ZacharyJia <jia199474@gmail.com>
2020-07-01 10:28:49 +0800

    travis ci setup

---
+++ travis/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+TRAVIS_OS_NAME=${1:-$TRAVIS_OS_NAME}
+CXX=${CXX:-g++}
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+	mkdir -p "$VCPKG_ROOT"
+	cd "$VCPKG_ROOT"
+	curl --retry 5 --connect-timeout 30 --location --remote-header-name --output installed.zip "$VCPKG_CACHE_ZIP_URL"
+	unzip -uo installed.zip > /dev/null
+	./vcpkg.exe integrate install
+else
+
+fi
