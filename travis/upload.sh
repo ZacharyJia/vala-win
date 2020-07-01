8d2c03e76d3b5d354bbf9d8c3636afe9ca2a8d27
ZacharyJia <jia199474@gmail.com>
2020-07-01 13:56:45 +0800

    fix wrong artifact filename

--- travis/upload.sh
+++ travis/upload.sh
@@ -6,7 +6,7 @@ if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
     cp -r ./vapi ./build/Release
     ls ./build/Release
     arc archive vala-prebuilt-$VALAC_VERSION-x64.zip ./build/Release/*
-    curl -F "file=@vala-prebuilt-x64.zip" https://api.anonfiles.com/upload
+    curl -F "file=@vala-prebuilt-$VALAC_VERSION-x64.zip" https://api.anonfiles.com/upload
 else
 	echo "not in this os"
 fi
